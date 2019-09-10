1f6826132802d498c22df33eb5ba886e5c5bc9cf
sam-malanchuk <malanchuksam@gmail.com>
2019-09-09 21:20:43 -0400

    follow along with lecture

--- users/users-router.js
+++ users/users-router.js
@@ -4,11 +4,21 @@ const Users = require('./users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
 
 router.get('/', restricted, (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
+  const { role, sub } = req.decodedToken;
+
+  if (role === 'admin') {
+    Users.find()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  } else {
+    Users.findById(sub)
+      .then(user => {
+        res.json(user);
+      })
+      .catch(err => res.send(err));
+  }
 });
 
 module.exports = router;
